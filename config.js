39de509e9bdc087be8c1a0d8f8b859b5c954a943
lvbowen <1010606892@qq.com>
2018-07-29 13:25:56 +0800

    ###

---
+++ config.js
@@ -0,0 +1,9 @@
+
+let config = {
+    // host:'https://aijuhr.com/hrm',       //正式
+    host:'https://aijutong.com/hrm',     //测试
+    appId:'wxcb2d86acf3a55170',
+    lversion:"1.0.0",
+}
+
+module.exports = config;
